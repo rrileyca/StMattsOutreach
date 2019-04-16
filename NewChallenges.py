a774660956755be1674a4c066ac9187fa078e841
callummcgraw <34253268+callummcgraw@users.noreply.github.com>
2019-04-16 19:49:31 -0400

    Update NewChallenges.py

--- NewChallenges.py
+++ NewChallenges.py
@@ -1,49 +1,49 @@
-Beginner Challenges
+#Beginner Challenges
 
-Challenge 1:
-Make a slash from 0-0 to 7-7 using calls to set_pixel()
+#Challenge 1:
+#Make a slash from 0-0 to 7-7 using calls to set_pixel()
 
-Challenge 2:
-Do the same, but using a FOR loop to use only one set_pixel() line
+#Challenge 2:
+#Do the same, but using a FOR loop to use only one set_pixel() line
 
-Challenge 3:
-Switch the slash from Challenge 2 to go from 0-7 to 7-0
+#Challenge 3:
+#Switch the slash from Challenge 2 to go from 0-7 to 7-0
 
-Challenge 4:
-Light one column using a FOR loop
+#Challenge 4:
+#Light one column using a FOR loop
 
-Challenge 5:
-Light one row using a FOR loop
+#Challenge 5:
+#Light one row using a FOR loop
 
-Intermediate Challenges
-Challenge 6:
-Make an oscillating dot, like Knight Rider, or a Cylon
+#Intermediate Challenges
+#Challenge 6:
+#Make an oscillating dot, like Knight Rider, or a Cylon
 
-Challenge 7:
-Make an oscillating column in a non-primary colour
+#Challenge 7:
+#Make an oscillating column in a non-primary colour
 
-Challenge 8:
-Make a fire truck light (light up one column at a time until all columns are on, then turn them off one column at a time)
+#Challenge 8:
+#Make a fire truck light (light up one column at a time until all columns are on, then turn them off one column at a time)
 
-Challenge 9:
-Make a police light, strobe between half blue half red, then the opposite
+#Challenge 9:
+#Make a police light, strobe between half blue half red, then the opposite
 
-Challenge 10:
-Light columns (or rows) 0,2,4 and 6 using only two FOR loops, using a range operator in the outer looop
+#Challenge 10:
+#Light columns (or rows) 0,2,4 and 6 using only two FOR loops, using a range operator in the outer looop
 
-Expert Challenges
-Challenge 11:
-In the examples folder, ascii_pic.py uses an arrany to set pixels on or off checking whether the array cell is blank or not.  Copy
-ascii_pic.py and change it so that you can set a pixels colour using 'R' 'G' 'B' in the array
+#Expert Challenges
+#Challenge 11:
+#In the examples folder, ascii_pic.py uses an arrany to set pixels on or off checking whether the array cell is blank or not.  Copy
+#ascii_pic.py and change it so that you can set a pixels colour using 'R' 'G' 'B' in the array
 
-Challenge 12:
-Take a colour as input from the user.  This could be either menu-based ("1 for Red, 2 for Blue) or text based ("Enter a colour:") Make sure you include an Exit option for either method.  Use the set_all() function to set the entire array their colour choice
+#Challenge 12:
+#Take a colour as input from the user.  This could be either menu-based ("1 for Red, 2 for Blue) or text based ("Enter a colour:") Make sure you include an Exit option for either method.  Use the set_all() function to set the entire array their colour choice
 
-Challenge 13:
-Make a rainbow in columns.  You can use set_pixel_hsv() to use Hue, which goes from 0.0 to 1.0.  Just divide 1 by the number of steps to make a full rainbow.
+#Challenge 13:
+#Make a rainbow in columns.  You can use set_pixel_hsv() to use Hue, which goes from 0.0 to 1.0.  Just divide 1 by the number of steps to make a full rainbow.
 
-Challenge 14:
-Use recursion to create a reducing square rainbow
+#Challenge 14:
+#Use recursion to create a reducing square rainbow
 
-Challenge 15:
-Make the colours scroll in Challenge 14
+#Challenge 15:
+#Make the colours scroll in Challenge 14
