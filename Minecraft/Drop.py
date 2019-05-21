5c644a9be77ec1f960846f0a464a240bacdd7289
callummcgraw <34253268+callummcgraw@users.noreply.github.com>
2019-05-21 16:24:34 -0400

    Create Drop.py

---
+++ Minecraft/Drop.py
@@ -0,0 +1,10 @@
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+import time
+
+mc = minecraft.Minecraft.create()
+
+playerPos = mc.player.getPos()
+mc.player.setPos(playerPos.x, playerPos.y + 50, playerPos.z)
+mc.postToChat("Dont look down")
+time.sleep(5)
