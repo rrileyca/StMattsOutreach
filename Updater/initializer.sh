a56136c53f7c924b36020cf742650a9365096ea3
Ryan Riley <rnriley@Ryans-MacBook-Pro.local>
2019-10-12 19:11:03 -0400

    fixed bashrc addition

--- Updater/initializer.sh
+++ Updater/initializer.sh
@@ -10,5 +10,9 @@ fi
 
 printf "\n### CLONING GIT REPO TO ~/OUTREACH\n"
 git clone https://github.com/Soxcks/StMattsOutreach.git $DIRECTORY
+
+# Clean up bashrc, then add updater to it
+sed -i '/\~\/Outreach\/Updater.*/d' .bashrc
 echo "Desktop/Outreach/Updater/updater.sh" >> ~/.bashrc
+
 printf "\nCompleted! :-)\n"
